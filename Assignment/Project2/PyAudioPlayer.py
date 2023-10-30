5a09a404977fb489668f6bdd21d02680f1796220
APSODE <2gunbo74@gmail.com>
2023-10-30 13:17:08 +0900

    비동기 구현 (1차)

--- Assignment/Project2/PyAudioPlayer.py
+++ Assignment/Project2/PyAudioPlayer.py
@@ -1,128 +1,92 @@
+from pyaudio import PyAudio, Stream
+
+from Assignment.Project2.decorator.NotNone import NotNone
+from Assignment.Project2.wrapper.AudioManager import AudioManager
+from Assignment.Project2.wrapper.LoadedAudioFile import LoadedAudioFile
+from matplotlib.pyplot import xlabel, ylabel, title, show
+from librosa.display import waveshow
 from typing import Optional
-from librosa import load
-from pyaudio import PyAudio
-from wave import open as wv_open
-from wave import Wave_read
+from concurrent.futures import ProcessPoolExecutor
+from asyncio import Task, create_task, sleep, run, to_thread, get_event_loop, AbstractEventLoop
+from threading import Thread
 
-
+import asyncio
 import os
-import matplotlib.pyplot as plt
-import librosa
 
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
-# Wave_read 객체에 대한 wrapper클래스
-class _LoadedAudioFile:
-    def __init__(self, wav_file_dir: str):
-        self._wav_file = wv_open(wav_file_dir, "rb")
-        self._librosa_tuple = load(wav_file_dir)
-        self._length = round(self._wav_file.getnframes() / self._wav_file.getframerate(), 2)
-
-    @staticmethod
-    def create_object(wav_file_dir: str) -> "_LoadedAudioFile":
-        return _LoadedAudioFile(wav_file_dir = wav_file_dir)
-
-    @property
-    def file(self) -> Wave_read:
-        return self._wav_file
-
-    @property
-    def length(self) -> float:
-        return self._length
-    
-    @property
-    def sampling(self) -> float:
-        return self._wav_file.getframerate() / 1000
-
-    @property
-    def librosa_tuple(self) -> tuple:
-        return self._librosa_tuple
-
-    def length_with_format(self) -> str:
-        minute = int(self._length // 60)
-        second = int(self._length - (60 * minute))
-
-        return f"{minute}분 {second}초"
-
-    def sampling_with_format(self):
-        return f"{self._wav_file.getframerate() / 1000}KHz"
-
-
-class PyAudioPlayer(PyAudio):
+class PyAudioPlayer:
     def __init__(self):
-        super().__init__()
         self._audio_source_dir = os.path.join(ROOT_PATH, "audio_sources")
-        self._loaded_file: Optional[_LoadedAudioFile] = None
-        self._dir_checker()
-        self.run()
-
-    def run(self):
-        self._menu()
+        self._loaded_file: Optional[LoadedAudioFile] = None
+        self._audio_manager: Optional[AudioManager] = None
+        self._is_playing: bool = False
+        asyncio.run(self._menu())
 
     def _load(self):
         input_audio_file_dir = os.path.join(self._audio_source_dir, input("input filename? : "))
-        self._loaded_file = _LoadedAudioFile.create_object(wav_file_dir = input_audio_file_dir)
+        self._loaded_file = LoadedAudioFile.create_object(wav_file_dir = input_audio_file_dir)
 
         print("Sampling rate = " + self._loaded_file.sampling_with_format())
+
         print("Play time = " + self._loaded_file.length_with_format())
 
-    def _play(self):
-        pass
+    @NotNone("_loaded_file")
+    async def _plot(self):
+        signal = self._loaded_file.librosa.signal
+        sampling_rate = self._loaded_file.librosa.sampling_rate
 
-    def _stop(self):
-        pass
+        start_time = int(int(input("start time ? :")) * sampling_rate)
+        end_time = int(int(input("end time ? :")) * sampling_rate)
 
-    def _r_play(self):
-        pass
+        waveshow(signal[start_time:end_time], sr=sampling_rate)
+        xlabel("Time (s)")
+        ylabel("Amplitude")
+        title("Audio Segment")
+        show()
 
-    def _plot(self):
-        y = self._loaded_file.librosa_tuple[0]
-        sr = self._loaded_file.librosa_tuple[1]
+    @NotNone("_loaded_file")
+    async def _play(self):
+        with AudioManager(loaded_audio_file = self._loaded_file) as audio_manager:
+            audio_manager: AudioManager
+            audio_manager.play()
+            self._audio_manager = audio_manager
 
-        start_time = int(int(input("start time ? :")) * sr)
-        end_time = int(int(input("end time ? :")) * sr)
+    @NotNone("_audio_manager")
+    async def _stop(self):
+        self._audio_manager.stop()
 
-        sliced_y = y[start_time:end_time]
-        # waveshow 함수가 matplotlib의 변경으로 인해 librosa의 현재 릴리즈 버전에서는
-        # matplotlib 3.8이상의 버전을 사용하면 예외가 발생한다.
-        # 따라서 matplotlib 3.7.3을 사용할 경우 정상적으로 출력이 가능한 것을 확인함.
-        librosa.display.waveshow(sliced_y, sr = sr)
-        plt.xlabel("Time (s)")
-        plt.ylabel("Amplitude")
-        plt.title("Audio Segment")
-        plt.show()
-
-    def _dir_checker(self):
-        if not os.path.exists(self._audio_source_dir):
-            os.mkdir(self._audio_source_dir)
-
-    def _menu(self):
+    async def _menu(self):
         while True:
             input_command = input("1. Load, 2. Play, 3. Stop, 4. R-Play, 5. Plot, 6. Exit ? ")
-            if input_command == "1":
-                self._load()
+            try:
+                if input_command == "1":
+                    self._load()
 
-            elif input_command == "2":
-                self._play()
+                elif input_command == "2":
+                    await self._play()
 
-            elif input_command == "3":
-                self._stop()
+                elif input_command == "3":
+                    await self._stop()
+                #
+                # elif input_command == "4":
+                #     self._r_play()
 
-            elif input_command == "4":
-                self._r_play()
+                elif input_command == "5":
+                    await self._plot()
 
-            elif input_command == "5":
-                self._plot()
+                elif input_command == "6":
+                    break
 
-            elif input_command == "6":
-                # break
-                pass
+                else:
+                    print("입력값이 잘못되었습니다. 다시 입력하십시오.")
 
-            else:
-                print("입력값이 잘못되었습니다. 다시 입력하십시오.")
+            except Exception as e:
+                print(e)
 
 
 if __name__ == '__main__':
     PAP = PyAudioPlayer()
+
